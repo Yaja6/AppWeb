92f14b06de45ba7bdf31f43488048146faeabb3d
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2021-03-09 02:28:26 -0500

    notificacion

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PublicationInterface } from 'src/app/models/publication.interface';
 import { UserInterface } from 'src/app/models/user.interface';
 import { AuthService } from 'src/app/services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-menu',
@@ -17,12 +19,17 @@ export class MenuComponent implements OnInit {
     password: '',
     rol: '',
   };
+  publications: PublicationInterface[] = [];
+  reports = true;
+  count = 0;
   constructor(
     private authSvc: AuthService,
-    private router: Router)
+    private router: Router,
+    public firestoreService: FirestoreService,)
     { }
 
   ngOnInit(): void {
+    this.getPublications();
   }
 
   goReports(){
@@ -50,6 +57,23 @@ export class MenuComponent implements OnInit {
       password: '',
       rol: '',
     };
+  }
+  getPublications(){
+    const path = 'Reports/';
+    this.firestoreService.getCollection<PublicationInterface>(path).subscribe( res => {  // res - respuesta del observador
+    if(res){
+      this.publications = res;
+      this.publications.forEach(e => {
+        if(e.state == 'Sin solucionar'){
+          this.count += 1;
+        }
+      });
+      
+    }
+    if(res.length === 0){
+      this.reports = false;
+    }
+   });
   }
 }
 
